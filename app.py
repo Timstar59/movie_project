90aa34df35a0b7fe4b9bcbf05ddf0180117d520d
Ubuntu <ubuntu@ip-172-31-0-81.eu-west-1.compute.internal>
2021-09-06 14:37:45 +0000

    added more python files to directory

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@123.45.6.78:3306/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
